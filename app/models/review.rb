efe1806efb6308af1f2611340edb4161a0aa4ae6
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-12 14:59:17 +0200

    yelp

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,4 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: (0..5) }
-  end
+end
